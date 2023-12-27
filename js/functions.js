1f2859601c7ce90dc3ff3a47d1a117443ea56ff0
FR_codes <74495215+Florisrietman@users.noreply.github.com>
2023-12-27 14:50:12 +0100

    Uploaded website files

---
+++ js/functions.js
@@ -0,0 +1,246 @@
+
+Amplitude.init({
+  "volume": 100,
+  "continue_next": false,
+  "songs": [
+    {
+      "name": "100%NL",
+      "artist": "De beste muziek van Nederland",
+      "url": "https://22613.live.streamtheworld.com/100PNL_AAC.aac",
+      "cover_art_url": "https://fr1234.neocities.org/100-NL.webp"
+    },
+    {
+      "name": "100%NL Feest",
+      "artist": "De leukste feesthits van Nederland",
+      "url": "https://25283.live.streamtheworld.com/WEB01_AAC.aac",
+      "cover_art_url": "https://scale.100p.nl/imageScaled/?site=100pnl&file=1561108081_Feest.jpg"
+    },
+    {
+      "name": "100%NL Liefde",
+      "artist": "Lovesongs",
+      "url": "https://25253.live.streamtheworld.com/WEB03_AAC.aac",
+      "cover_art_url": "https://scale.100p.nl/imageScaled/?site=100pnl&file=1561108123_Liefde.jpg"
+    },
+    {
+      "name": "100%NL Non-stop",
+      "artist": "De beste muziek van Nederland non-stop",
+      "url": "https://25283.live.streamtheworld.com/WEB02_AAC.aac",
+      "cover_art_url": "https://scale.100p.nl/imageScaled/?site=100pnl&file=1561108141_Nonstop.jpg"
+    },
+    {
+      "name": "100%NL Nederpop",
+      "artist": "Nederlandstalig Pop/Hits",
+      "url": "https://25233.live.streamtheworld.com/WEB04_AAC.aac",
+      "cover_art_url": "https://scale.100p.nl/imageScaled/?site=100pnl&file=1561109013_nederpop.png"
+    },
+    {
+      "name": "538",
+      "artist": "The Beat Of The Moment - Pop/Hits",
+      "url": "https://22713.live.streamtheworld.com/RADIO538.mp3",
+      "cover_art_url": "https://fr1234.neocities.org/538.webp"
+    },
+    {
+      "name": "538 Non-stop",
+      "artist": "Een station, alle hits, non-stop! - Pop/Hits",
+      "url": "https://25293.live.streamtheworld.com/TLPSTR09.mp3",
+      "cover_art_url": "https://fr1234.neocities.org/538-1.webp"
+    },
+    {
+      "name": "538 Zomer",
+      "artist": "Zomerhits",
+      "url": "https://25303.live.streamtheworld.com/TLPSTR06.mp3",
+      "cover_art_url": "https://fr1234.neocities.org/538-2.webp"
+    },
+    {
+      "name": "538 Top 50",
+      "url": "https://25233.live.streamtheworld.com/TLPSTR13.mp3",
+      "cover_art_url": "https://fr1234.neocities.org/538-3.webp"
+    },
+    {
+      "name": "538 Classics",
+      "artist": "Oldies/Classics",
+      "url": "https://25353.live.streamtheworld.com/TLPSTR08.mp3",
+      "cover_art_url": "https://fr1234.neocities.org/538-4.webp"
+    },
+    {
+      "name": "538 Dance Department",
+      "artist": "The best new dance music - Dance",
+      "url": "https://22543.live.streamtheworld.com/TLPSTR01.mp3",
+      "cover_art_url": "https://fr1234.neocities.org/538-5.webp"
+    },
+    {
+      "name": "538 Ibiza",
+      "url": "https://22343.live.streamtheworld.com/TLPSTR19.mp3",
+      "cover_art_url": "https://fr1234.neocities.org/538-6.webp"
+    },
+    {
+      "name": "Joe 70s & 80s",
+      "url": "https://icecast-qmusicnl-cdp.triple-it.nl/Joe_nl.mp3",
+      "cover_art_url": "https://fr1234.neocities.org/Joe-4.webp"
+    },
+    {
+      "name": "Joe 80s & 90s",
+      "url": "https://icecast-qmusicbe-cdp.triple-it.nl/joe_80s_90s.mp3",
+      "cover_art_url": "https://fr1234.neocities.org/Joe-1.webp"
+    },
+    {
+      "name": "Joe 60s & 70s",
+      "url": "https://icecast-qmusicbe-cdp.triple-it.nl/joe_60s_70s.mp3",
+      "cover_art_url": "https://fr1234.neocities.org/Joe-2.webp"
+    },
+    {
+      "name": "Joe Lage Landen",
+      "url": "https://icecast-qmusicbe-cdp.triple-it.nl/joe_lage_landen.mp3",
+      "cover_art_url": "https://fr1234.neocities.org/Joe-3.webp"
+    },
+    {
+      "name": "Joe Easy",
+      "url": "https://icecast-qmusicbe-cdp.triple-it.nl/joe_easy.mp3",
+      "cover_art_url": "https://fr1234.neocities.org/Joe-5.webp"
+    },
+    {
+      "name": "Joe Top 2000",
+      "url": "https://icecast-qmusicbe-cdp.triple-it.nl/joe_top2000.mp3",
+      "cover_art_url": "https://fr1234.neocities.org/Joe-6.webp"
+    },
+    {
+      "name": "JoyRadio Groningen",
+      "url": "https://stream.joyradio.nl/joyradio-gron",
+      "cover_art_url": "https://fr1234.neocities.org/joyradio1.png"
+    },
+    {
+      "name": "JoyRadio Friesland/Drenthe",
+      "url": "https://stream.joyradio.nl/joyradio",
+      "cover_art_url": "https://fr1234.neocities.org/joyradio2.png"
+    },
+    {
+      "name": "JoyRadio Twente",
+      "url": "https://stream.joyradio.nl/joyradio-twente",
+      "cover_art_url": "https://fr1234.neocities.org/joyradio3.png"
+    },
+    {
+      "name": "JoyRadio Hoogeveen",
+      "url": "https://stream.joyradio.nl/joyradio-hoogeveen",
+      "cover_art_url": "https://fr1234.neocities.org/joyradio4.png"
+    },
+    {
+      "name": "NPO Radio 1",
+      "url": "https://icecast.omroep.nl/radio1-bb-mp3",
+      "cover_art_url": "https://fr1234.neocities.org/NPO_1.jpg"
+    }
+  ],
+   visualizations: [
+      {
+        object: MichaelBromleyVisualization,
+        params: {
+
+        }
+      }
+    ]
+});
+/*
+  Shows the playlist
+*/
+document.getElementsByClassName('show-playlist')[0].addEventListener('click', function(){
+  document.getElementById('white-player-playlist-container').classList.remove('slide-out-top');
+  document.getElementById('white-player-playlist-container').classList.add('slide-in-top');
+  document.getElementById('white-player-playlist-container').style.display = "block";
+});
+
+/*
+  Hides the playlist
+*/
+document.getElementsByClassName('close-playlist')[0].addEventListener('click', function(){
+  document.getElementById('white-player-playlist-container').classList.remove('slide-in-top');
+  document.getElementById('white-player-playlist-container').classList.add('slide-out-top');
+  document.getElementById('white-player-playlist-container').style.display = "none";
+});
+
+/*
+  Gets all of the add to playlist buttons so we can
+  add some event listeners to actually add to playlist.
+*/
+var addToPlaylistButtons = document.getElementsByClassName('add-to-playlist-button');
+
+for( var i = 0; i < addToPlaylistButtons.length; i++ ){
+  /*
+    Add an event listener to the add to playlist button.
+  */
+  addToPlaylistButtons[i].addEventListener('click', function(){
+    var songToAddIndex = this.getAttribute('song-to-add');
+
+    /*
+      Adds the song to Amplitude, appends the song to the display,
+      then rebinds all of the AmplitudeJS elements.
+    */
+    var newIndex = Amplitude.addSong( songsToAdd[ songToAddIndex ] );
+    appendToSongDisplay( songsToAdd[ songToAddIndex ], newIndex );
+    Amplitude.bindNewElements();
+
+    /*
+      Removes the container that contained the add to playlist button.
+    */
+    var songToAddRemove = document.querySelector('.song-to-add[song-to-add="'+songToAddIndex+'"]');
+    songToAddRemove.parentNode.removeChild( songToAddRemove );
+  });
+}
+
+/*
+  Appends the song to the display.
+*/
+function appendToSongDisplay( song, index ){
+  /*
+    Grabs the playlist element we will be appending to.
+  */
+  var playlistElement = document.querySelector('.white-player-playlist');
+
+  /*
+    Creates the playlist song element
+  */
+  var playlistSong = document.createElement('div');
+  playlistSong.setAttribute('class', 'white-player-playlist-song amplitude-song-container amplitude-play-pause');
+  playlistSong.setAttribute('data-amplitude-song-index', index);
+
+  /*
+    Creates the playlist song image element
+  */
+  var playlistSongImg = document.createElement('img');
+  playlistSongImg.setAttribute('src', song.cover_art_url);
+
+  /*
+    Creates the playlist song meta element
+  */
+  var playlistSongMeta = document.createElement('div');
+  playlistSongMeta.setAttribute('class', 'playlist-song-meta');
+
+  /*
+    Creates the playlist song name element
+  */
+  var playlistSongName = document.createElement('span');
+  playlistSongName.setAttribute('class', 'playlist-song-name');
+  playlistSongName.innerHTML = song.name;
+
+  /*
+    Creates the playlist song artist album element
+  */
+  var playlistSongArtistAlbum = document.createElement('span');
+  playlistSongArtistAlbum.setAttribute('class', 'playlist-song-artist');
+  playlistSongArtistAlbum.innerHTML = song.artist+' &bull; '+song.album;
+
+  /*
+    Appends the name and artist album to the playlist song meta.
+  */
+  playlistSongMeta.appendChild( playlistSongName );
+  playlistSongMeta.appendChild( playlistSongArtistAlbum );
+
+  /*
+    Appends the song image and meta to the song element
+  */
+  playlistSong.appendChild( playlistSongImg );
+  playlistSong.appendChild( playlistSongMeta );
+
+  /*
+    Appends the song element to the playlist
+  */
+  playlistElement.appendChild( playlistSong );
+}
