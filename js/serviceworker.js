7a152d0ea46aa7615a64a53ea3a16661388860f2
FR1234 <florisrietman@gmail.com>
2023-12-31 12:50:05 +0100

    Update serviceworker.js

--- js/serviceworker.js
+++ js/serviceworker.js
@@ -1,9 +1,7 @@
 const urlsToCache = ["/", "https://blissradio.github.io/js/amplitude.js", "https://cdn.jsdelivr.net/npm/amplitudejs@5.3.2/dist/visualizations/michaelbromley.js", "https://blissradio.github.io/css/app.css", "https://blissradio.github.io/js/functions.js", "https://blissradio.github.io/img/previous.svg", "https://blissradio.github.io/img/stop.svg", "https://blissradio.github.io/img/next.svg", "https://blissradio.github.io/img/thumb.png", "https://blissradio.github.io/img/stop1.svg", "https://fonts.gstatic.com/s/lexend/v19/wlptgwvFAVdoq2_F94zlCfv0bz1WCzsWzLdneg.woff2", "https://fonts.gstatic.com/s/lato/v24/S6uyw4BMUTPHjx4wXg.woff2", "https://blissradio.github.io/img/minimize.svg", "https://blissradio.github.io/img/link.svg", "https://blissradio.github.io/img/volume.svg", "https://blissradio.github.io/img/logo3.png", "https://blissradio.github.io/img/maximize.svg", "https://blissradio.github.io/img/100-NL.webp", "https://blissradio.github.io/img/leeuwicon_orange.png", "https://blissradio.github.io/img/538.webp", "https://blissradio.github.io/img/favicon.png", "https://fr1234.neocities.org/100-NL.webp", "https://blissradio.github.io/img/play.svg", "https://blissradio.github.io/img/volume1.svg", "https://blissradio.github.io/manifest.json", "https://blissradio.github.io/favicon.ico", "https://blissradio.github.io/logo2.png", "https://blissradio.github.io/desktop1.png", "https://blissradio.github.io/desktop2.png", "https://blissradio.github.io/desktop3.png", "https://blissradio.github.io/img/538-1.webp", "https://blissradio.github.io/img/538-2.webp", "https://blissradio.github.io/img/538-3.webp", "https://blissradio.github.io/img/538-4.webp", "https://blissradio.github.io/img/538-5.webp", "https://blissradio.github.io/img/538-6.webp", "https://blissradio.github.io/img/Joe-4.webp", "https://blissradio.github.io/img/Joe-1.webp", "https://blissradio.github.io/img/Joe-2.webp", "https://blissradio.github.io/img/Joe-3.webp", "https://blissradio.github.io/img/Joe-5.webp", "https://blissradio.github.io/img/Joe-6.webp", "https://blissradio.github.io/img/joyradio1.webp", "https://blissradio.github.io/img/logo(2).webp", "https://blissradio.github.io/img/joyradio2.webp", "https://blissradio.github.io/img/joyradio3.webp", "https://blissradio.github.io/img/joyradio4.webp", "https://blissradio.github.io/img/NPO_1.webp"];
-self.addEventListener("install", event => {
-   event.waitUntil(
-      caches.open("pwa-assets")
-      .then(cache => {
-         return cache.addAll(urlsToCache);
-      });
-   );
+self.addEventListener("install", (event) => {
+   event.waitUntil((async () => {
+      const cache = await caches.open("pwa-assets");
+      return cache.addAll(urlsToCache);
+   })());
 });
